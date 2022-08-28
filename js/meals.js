2abebca06c3e9740773176c1ade875baaf591430
MrMernDeveloper <rakibkst16@gmail.com>
2022-08-28 19:27:28 +0600

    added some code

--- js/meals.js
+++ js/meals.js
@@ -10,15 +10,15 @@ const displayMeals = meals => {
     const mealsContainer = document.getElementById('meal-container');
     mealsContainer.innerHTML = ``
     for (const meal of meals) {
-        // console.log(meal)
+        //  console.log(meal)
         const mealDiv = document.createElement('div')
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card">
+        <div class="card" onclick="displayMealDetails(${meal.idMeal})">
                         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${meal.strMeal}</h5>
-                            <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+                            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
                         </div>
                     </div>
         `
@@ -31,6 +31,33 @@ const searchMeals = () => {
     const searchText = inputField.value;
     loadMeals(searchText);
     inputField.value = '';
-   
+
 }
 
+const displayMealDetails = (idMeals) => {
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeals}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAnotherMeals(data.meals[0]))
+}
+
+const displayAnotherMeals = (details) => {
+    console.log(details)
+    const displaycontainer = document.getElementById('mealdetails')
+    displaycontainer.innerHTML = ``
+    const mealDiv = document.createElement('div')
+    mealDiv.classList.add('card')
+    mealDiv.innerHTML = `
+     <div>
+     <img src="${details.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${details.strMeal}
+                        <h6>Menu ID:${details.idMeal}</h6>                        <p class="card-text">${details.strInstructions.slice(0, 200)}</p>
+                    </div>
+                </div>
+    `
+    displaycontainer.appendChild(mealDiv);
+
+
+}
